290f766fc96c7660244e8ae44ed4ae59a61a03da
Carsten Gasch <carsten.gasch@gmail.com>
2024-03-10 16:44:19 +0100

    messaging for intents of movement

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,6 +2,7 @@ mod collisions;
 mod end_turn;
 mod entity_render;
 mod map_render;
+mod movement;
 mod player_input;
 mod random_move;
 
@@ -9,7 +10,7 @@ use crate::prelude::*;
 
 use self::{
     collisions::collisions_system, end_turn::end_turn_system, entity_render::entity_render_system,
-    map_render::map_render_system, player_input::player_input_system,
+    map_render::map_render_system, movement::movement_system, player_input::player_input_system,
     random_move::random_move_system,
 };
 
@@ -24,6 +25,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement_system())
+        .flush()
         .add_system(collisions_system())
         .flush()
         .add_system(map_render_system())
@@ -35,6 +38,8 @@ pub fn build_player_scheduler() -> Schedule {
 pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move_system())
+        .flush()
+        .add_system(movement_system())
         .flush()
         .add_system(collisions_system())
         .flush()
